0fa557f34cada03623c30eeb70261b464c0e3d45
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-09-22 20:12:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Purchase order domain fix
+-----------------------------
+This module add a suitable filter on partner so that the user can select only suppliers (taking in account how odoo consider partner as supplier).
+
+
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Purchase order domain fix',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Purchase',
+    'summary': 'Purchase order domain fix',
+    'description': "",
+    'depends': [
+        'purchase'
+    ],
+    'data': [
+        'views/purchase_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
